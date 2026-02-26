3c4f34b0b2d2424e1df001372700230fefaa2ac3
JM | José Matheus <Jmatheus_vida@outlook.com>
2026-02-26 16:19:32 -0300

    add: new sound

--- src/modules/audioManager.js
+++ src/modules/audioManager.js
@@ -2,7 +2,8 @@ const sounds = {
     click: new Audio('src/assets/sounds/click.mp3'),
     window: new Audio('src/assets/sounds/window.mp3'),
     menu: new Audio('src/assets/sounds/menuDesk.mp3'),
-    clippy: new Audio('src/assets/sounds/clippy.mp3')
+    clippy: new Audio('src/assets/sounds/clippy.mp3'),
+    minimize: new Audio('src/assets/sounds/minimize.mp3')
 };
 
 Object.values(sounds).forEach(audio => audio.volume = 0.5);
